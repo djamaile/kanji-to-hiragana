a116871352469adfa761a91f4ea38fcfd79c5b30
djamaile <rdjamaile@gmail.com>
2020-08-06 14:44:26 +0200

    deploy

---
+++ build/precache-manifest.d6c836956df56351888a9dc5439541d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca825fba15b52d939db45e0cd22b43d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bf1fde3fc2e703e2909",
+    "url": "/static/css/main.5685fcc9.chunk.css"
+  },
+  {
+    "revision": "abd2d45ec4cb1928889e",
+    "url": "/static/js/2.b084e95b.chunk.js"
+  },
+  {
+    "revision": "4bf1fde3fc2e703e2909",
+    "url": "/static/js/main.710a518d.chunk.js"
+  },
+  {
+    "revision": "e8007b6316b22387850b",
+    "url": "/static/js/runtime-main.5da9ab25.js"
+  }
+]);
