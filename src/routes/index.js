1a686b869a8e2aa0ca0d026c4192a2ffe06e9b66
djamaile <rdjamaile@gmail.com>
2020-08-06 15:04:33 +0200

    added browser router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,15 @@
 import React, { Fragment } from "react";
-import { Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import history from "../utils/history";
 import Home from "./home";
 
 const AppRouter = () => (
     <Fragment>
-        <Router history={history} basename={window.location.pathname || ''}>
+        <BrowserRouter history={history} basename={window.location.pathname || ''}>
             <Switch>
                 <Route exact path="/" component={Home} />
             </Switch>
-        </Router>
+        </BrowserRouter>
     </Fragment>
 );
 
