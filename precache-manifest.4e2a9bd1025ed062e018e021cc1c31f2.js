2cf8750d41eb528a7f16f749f8c4704dd0293fe3
djamaile <djamaile@users.noreply.github.com>
2020-08-06 12:40:52 +0000

    deploy: b98c1cc3569e3f15e0ded04cb1e84efd3e723d70

---
+++ precache-manifest.4e2a9bd1025ed062e018e021cc1c31f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e0cc41a9329db7f857aa46b61b1cc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfbad5ff2d832e2c9499",
+    "url": "/static/css/main.5685fcc9.chunk.css"
+  },
+  {
+    "revision": "abd2d45ec4cb1928889e",
+    "url": "/static/js/2.b084e95b.chunk.js"
+  },
+  {
+    "revision": "dfbad5ff2d832e2c9499",
+    "url": "/static/js/main.ed1dbb01.chunk.js"
+  },
+  {
+    "revision": "e8007b6316b22387850b",
+    "url": "/static/js/runtime-main.5da9ab25.js"
+  }
+]);
